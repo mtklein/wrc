382379cbc3bba38c59158f03140d7d0d97bea45c
Mike Klein <mtklein@chromium.org>
2016-08-18 09:52:09 -0400

    no need to read dst

--- wrc.cc
+++ wrc.cc
@@ -3,11 +3,10 @@
 // This function is thread safe with respect to weight but not with respect to src or dst.
 // It should never need to be thread-safe with respect to dst... src is still in question.
 void split_shares(size_t* src, size_t* dst, std::atomic<size_t>* weight) {
-    size_t s = *src,
-           d = *dst;
+    size_t s = *src;
 
     if (__builtin_expect(s > 1, true)) {
-        d = s / 2;
+        size_t d = s / 2;
         s -= d;
         *dst = d;
         *src = s;
